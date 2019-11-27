3fecf67a0d06b97cb15fa3b98818f324992ba305
whaoran <whaoran@seas.upenn.edu>
2019-11-26 20:15:14 -0500

    base code

---
+++ Source/HandOfGod/Hexasphere.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hexasphere.h"
+
+Hexasphere::Hexasphere()
+{
+}
+
+Hexasphere::~Hexasphere()
+{
+}
---
+++ Source/HandOfGod/Hexitile.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hexitile.h"
+
+// Sets default values
+AHexitile::AHexitile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHexitile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHexitile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
